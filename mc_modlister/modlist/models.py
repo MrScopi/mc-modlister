c29d110a9af47d6d7cbb7693c7faee5f8fc2dfff
Scott King <kingscott@gmail.com>
2025-06-26 20:37:35 +0000

    new model with migrations

--- mc_modlister/modlist/models.py
+++ mc_modlister/modlist/models.py
@@ -1,24 +1,30 @@
 from django.db import models
 
-class VersionType(models.TextChoices):
-    
-    RELEASE = 'release', 'Release'
-    BETA = 'beta', 'Beta'
-    ALPHA = 'alpha', 'Alpha'
+VERSION_TYPE = [    
+    ('release', 'Release'),
+    ('beta', 'Beta'),
+    ('alpha', 'Alpha'),
+]
+
+MC_VERSIONS = [
+    ('1.21.5', '1.21.5'),
+    ('1.21.6', '1.21.6'),
+    ('1.21.7', '1.21.7'),
+]  
 
 class TrackedMods(models.Model):
 
     # Data we store, not fetch at refresh
-    project_id = models.CharField(null=True)
+    project_id = models.CharField(null=True, max_length=8)
     mod_name = models.CharField(null=True)
     installed_version = models.CharField(null=True)
-    installed_version_type = models.CharField(null=True, choices=VersionType.choices)
-    installed_mc_version = models.CharField(null=True)
+    installed_version_type = models.CharField(null=True, choices=VERSION_TYPE)
+    installed_mc_version = models.CharField(null=True, choices=MC_VERSIONS)
 
     # Data we fetch at refresh
     latest_version = models.CharField(null=True)
-    latest_version_type = models.CharField(null=True, choices=VersionType.choices)
-    latest_mc_version = models.CharField(null=True)
+    latest_version_type = models.CharField(null=True, choices=VERSION_TYPE)
+    latest_mc_version = models.CharField(null=True, choices=MC_VERSIONS)
     last_updated = models.DateTimeField(null=True)
 
     def __str__(self):
