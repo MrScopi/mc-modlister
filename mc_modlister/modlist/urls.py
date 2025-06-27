589be43992999a9934f4ff436b06b0232b419a14
Scott King <kingscott@gmail.com>
2025-06-27 18:49:06 +0000

    add a Delete view and link/js function

--- mc_modlister/modlist/urls.py
+++ mc_modlister/modlist/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import ModListView, ModCreateAPIView, ModListAPIView
+from .views import ModListView, ModCreateAPIView, ModListAPIView, ModDestroyAPIView
 
 urlpatterns = [
     path('', ModListView.as_view(), name='list'),
     path('api/add', ModCreateAPIView.as_view(), name='api_add'),
     path('api/list', ModListAPIView.as_view(), name='api_list'),
+    path('api/destroy/<str:pk>', ModDestroyAPIView.as_view(), name='api_remove'),
 ]
--- mc_modlister/modlist/views.py
+++ mc_modlister/modlist/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.urls import reverse_lazy
 
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, UpdateAPIView
 
 from .models import TrackedMods
 from .serializers import TrackedModsSerializer
@@ -16,5 +16,9 @@ class ModCreateAPIView(CreateAPIView):
     serializer_class = TrackedModsSerializer
 
 class ModListAPIView(ListAPIView):
+    queryset = TrackedMods.objects.all()
+    serializer_class = TrackedModsSerializer
+
+class ModDestroyAPIView(DestroyAPIView):
     queryset = TrackedMods.objects.all()
     serializer_class = TrackedModsSerializer
